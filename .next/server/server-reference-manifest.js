ef81f3ab43e0c86297c7cd2853ca8bfe4988cdeb
Dave Donnelly <davepauldonnelly@gmail.com>
2025-06-17 16:25:38 +1000

    Remove build folder from repo

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fFGK0S7wlZPRipDtOoC1AFdwNX5KSHkaGuQqTuusX48=\"\n}"
--- .next/static/chunks/src_app_page_034b81c5.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_37f3d132._.css",
-  "static/chunks/_92de036e._.js"
-],
-    source: "dynamic"
-});
